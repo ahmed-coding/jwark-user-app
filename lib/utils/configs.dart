4b40b1a785bf817dfe1a8fab7abe8179966bd92e
Ahmed M Hamzah <71948774+ahmed-coding@users.noreply.github.com>
2026-02-28 00:07:21 +0300

    delete the slash from the end of url

--- lib/utils/configs.dart
+++ lib/utils/configs.dart
@@ -8,7 +8,7 @@ var defaultPrimaryColor = Color(0xFF5F60B9);
 // Don't add slash at the end of the url
 
 // const DOMAIN_URL = "http://192.168.0.177";
-const DOMAIN_URL = "https://ghostwhite-leopard-823170.hostingersite.com/";
+const DOMAIN_URL = "https://ghostwhite-leopard-823170.hostingersite.com";
 
 const BASE_URL = '$DOMAIN_URL/api/';
 
